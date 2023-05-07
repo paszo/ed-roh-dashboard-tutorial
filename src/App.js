64601f4622031b7e6a6a4c2c0e3336a3dee1d64f
paszo <paszo2008@gmail.com>
2023-05-07 16:47:53 +0200

    added icons to the sidebar

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline/>
             <div className="app">
+                <Sidebar></Sidebar>
                 <main className="content">
                     <Topbar/>
                     <Routes>
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,23 @@
+import {useState} from "react";
+import {ProSidebar, Menu, MenuItem} from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Box, IconButton, Typography, useTheme} from "@mui/material";
+import { Link } from "react-router-dom";
+import {tokens} from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+
 const Sidebar = () => {
     return <div>Sidebar</div>
 }
