51af7bdc8570f4debde390e637a526ad66be0381
paszo <paszo2008@gmail.com>
2023-05-07 19:09:15 +0200

    added user section to the sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -19,7 +19,57 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 
 const Sidebar = () => {
-    return <div>Sidebar</div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState('Dashboard');
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+                }
+            }}
+        >
+            <Menu>
+                {/* USER */}
+                {!isCollapsed && (
+                    <Box mb="25px">
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <img
+                                alt="profile-user"
+                                width="100px"
+                                height="100px"
+                                src={`../../assets/user.png`}
+                                style={{cursor: "pointer", borderRadius: "50%"}}
+                            />
+
+                        </Box>
+                        <Box textAlign="center">
+                            <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{m: "10px 0 0 0"}}>Ed Roh</Typography>
+                            <Typography variant="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                        </Box>
+                    </Box>
+                )}
+
+
+
+            </Menu>
+
+        </Box>
+    )
 }
 
 export default Sidebar;
