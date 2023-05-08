93889aa3bcbaa9afdbfb4f5582d211a528a702eb
paszo <paszo2008@gmail.com>
2023-05-08 16:17:28 +0200

    added invoices

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
@@ -31,7 +31,7 @@ function App() {
                         <Route path="/" element={<Dashboard/>}/>
                         <Route path="/team" element={<Team/>}/>
                         <Route path="/contacts" element={<Contacts/>}/>
-                        {/*<Route path="/invoices" element={<Invoices/>}/>*/}
+                        <Route path="/invoices" element={<Invoices/>}/>
                         {/*<Route path="/form" element={<Form/>}/>*/}
                         {/*<Route path="/bar" element={<Bar/>}/>*/}
                         {/*<Route path="/pie" element={<Pie/>}/>*/}
--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,29 +1,29 @@
-import {Box} from "@mui/material";
-import {DataGrid, GridToolbar} from "@mui/x-data-grid";
+import {Box, Typography, useTheme} from "@mui/material";
+import {DataGrid} from "@mui/x-data-grid";
 import {tokens} from "../../theme";
-import {mockDataContacts} from "../../data/mockData";
+import {mockDataInvoices} from "../../data/mockData";
 import Header from "../../components/Header";
-import {useTheme} from "@mui/material";
-const Contacts = () => {
+const Invoices = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     const columns = [
-        {field: "id", headerName: "ID", flex: 0.5},
-        {field: "registrarId", headerName: "Registrar ID"},
+        {field: "id", headerName: "ID"},
         {field: "name", headerName: "Name", flex: 1, cellClassName: "name-column-cell"},
-        {field: "age", headerName: "Age", type: "number", headerAlign: "left", align: "left", cellClassName: "name-column-cell"},
         {field: "phone", headerName: "Phone Number"},
-        {field: "email", headerName: "Email"},
-        {field: "address", headerName: "Address"},
-        {field: "city", headerName: "City"},
-        {field: "zipCode", headerName: "ZipCode"}
+        {field: "cost", headerName: "Cost", flex: 1, renderCell: (params) => (
+            <Typography color={colors.greenAccent[500]}>
+                ${params.row.cost}
+            </Typography>
+            )},
+        {field: "date", headerName: "Date"},
+        {field: "email", headerName: "Email"}
     ]
 
 
     return (
         <Box m="20px">
-            <Header title="CONTACTS" subtitle="List of Contacts for Future Reference"/>
+            <Header title="INVOICES" subtitle="List of Invoice Balances"/>
             <Box
             m="40px 0 0 0"
             height = "75vh"
@@ -48,15 +48,16 @@ const Contacts = () => {
                     borderTop: "none",
                     backgroundColor: colors.blueAccent[700]
                 },
-                "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-                    color: `${colors.grey[100]} !important`
+
+                "& .MuiCheckbox-root": {
+                    color: `${colors.greenAccent[200]} !important`
                 }
             }}
             >
                 <DataGrid
-                rows={mockDataContacts}
+                    checkboxSelection
+                rows={mockDataInvoices}
                 columns={columns}
-                components={{Toolbar: GridToolbar}}
                 >
 
                 </DataGrid>
@@ -65,4 +66,4 @@ const Contacts = () => {
     )
 }
 
-export default Contacts;
+export default Invoices;
