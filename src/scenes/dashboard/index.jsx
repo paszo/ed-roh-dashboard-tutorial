a8a47b9a77a2e6b010731253f0baf43fee462795
paszo <paszo2008@gmail.com>
2023-05-08 14:31:37 +0200

    added the header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header';
+import {Box} from "@mui/material";
 
 const Dashboard = () => {
     return (
-        <div>
+        <Box  m="20px">
+            <Box dispaly="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subtitle="Welcome to your dashboard"></Header>
+            </Box>
 
-        </div>
+        </Box>
     );
 };
 
