5b6ac0d419aeb5aa9229e3a648efc9eede0d1b8d
paszo <paszo2008@gmail.com>
2023-05-10 21:09:13 +0200

    callendar before adding missing dependency

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from './scenes/calendar';
+import Calendar from './scenes/calendar';
 
 function App() {
 
@@ -38,7 +38,7 @@ function App() {
                         {/*<Route path="/line" element={<Line/>}/>*/}
                         {/*<Route path="/faq" element={<FAQ/>}/>*/}
                         {/*<Route path="/geography" element={<Geography/>}/>*/}
-                        {/*<Route path="/calendar" element={<Calendar/>}/>*/}
+                        <Route path="/calendar" element={<Calendar/>}/>
                     </Routes>
                 </main>
             </div>
