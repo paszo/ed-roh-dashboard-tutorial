6903ea1ca8a42c4235bd3b92a8a585e1be331c32
paszo <paszo2008@gmail.com>
2023-05-11 17:58:05 +0200

    added geo chart

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from './scenes/calendar';
 
 function App() {
@@ -37,7 +37,7 @@ function App() {
                         <Route path="/pie" element={<Pie/>}/>
                         <Route path="/line" element={<Line/>}/>
                         <Route path="/faq" element={<FAQ/>}/>
-                        {/*<Route path="/geography" element={<Geography/>}/>*/}
+                        <Route path="/geography" element={<Geography/>}/>
                         <Route path="/calendar" element={<Calendar/>}/>
                     </Routes>
                 </main>
---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,22 @@
+import {Box} from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/GeographyChart";
+import {tokens} from "../../theme";
+import {useTheme} from "@mui/material";
+const Geography = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box m="20xp">
+           <Header title="Geography Chart" subtitle="Simple Geography Chart"/>
+            <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius="4px">
+                <GeographyChart>
+
+                </GeographyChart>
+            </Box>
+        </Box>
+    )
+}
+
+export default Geography;
