f6d8984e99fdf28862bac870540cd684723475b5
paszo <paszo2008@gmail.com>
2023-05-11 14:50:10 +0200

    added barchart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
@@ -33,7 +33,7 @@ function App() {
                         <Route path="/contacts" element={<Contacts/>}/>
                         <Route path="/invoices" element={<Invoices/>}/>
                         <Route path="/form" element={<Form/>}/>
-                        {/*<Route path="/bar" element={<Bar/>}/>*/}
+                        <Route path="/bar" element={<Bar/>}/>
                         {/*<Route path="/pie" element={<Pie/>}/>*/}
                         {/*<Route path="/line" element={<Line/>}/>*/}
                         <Route path="/faq" element={<FAQ/>}/>
